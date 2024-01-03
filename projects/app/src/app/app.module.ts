9acf5fdb9ff3555fe8c601736b16fb894c5f2166
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-01-03 12:39:50 +0200

    removed abandoned code

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { CustomRouter } from 'custom-router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -10,7 +9,6 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
-import { Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 export function getBaseHref(platformLocation: PlatformLocation): string {
@@ -20,7 +18,6 @@ export function getBaseHref(platformLocation: PlatformLocation): string {
 
 @NgModule({
     providers: [
-      { provide: Router, useClass: CustomRouter },
       {
           provide: APP_BASE_HREF,
           useFactory: getBaseHref,
