f73bfcf17c50973beb5c03a54c6389d6493373b2
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-01-19 18:25:53 +0200

    added bufferize middleware

---
+++ projects/supervisor/src/lib/buffer.ts
@@ -0,0 +1,41 @@
+import { Action } from "redux-replica";
+import { Lock } from "redux-sequential";
+import { filter, firstValueFrom, take } from "rxjs";
+
+// Define your higher-order function
+export const createBufferize = (lock: Lock) => {
+  const bufferize = ({ dispatch, getState, isDispatching } : any) => (next: Function) => async (action: Action<any>) => {
+    const isChildAction = action.meta && action.meta.child;
+
+    if(isChildAction) {
+      // If it's a child action, process it immediately
+      next(action);
+    } else {
+      // If it's a parent action, acquire the lock
+      await lock.acquire();
+
+      try {
+        // Wait until isDispatching is false
+        await firstValueFrom((isDispatching).pipe(
+          filter((value) => value === false),
+          take(1)
+        ));
+
+        // Process the parent action
+        next(action);
+      } finally {
+        // Release the lock
+        lock.release();
+      }
+    }
+  };
+
+  // Return the bufferize middleware
+  return bufferize;
+};
+
+// Create a new lock
+const lock = new Lock();
+
+// Create the bufferize middleware with the lock
+export const bufferize = createBufferize(lock);
--- projects/supervisor/src/public-api.ts
+++ projects/supervisor/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of supervisor
  */
 
+export * from './lib/buffer';
 export * from './lib/effects';
 export * from './lib/module';
 export * from './lib/supervisor';
