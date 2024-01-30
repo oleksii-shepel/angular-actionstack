251e27ed5866d81ff866606240a96745aa064b71
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-01-30 08:42:56 +0200

    corrections

--- projects/supervisor/src/lib/buffer.ts
+++ projects/supervisor/src/lib/buffer.ts
@@ -1,18 +1,21 @@
 import { Lock } from "redux-sequential";
 import { ActionQueue } from "./collections";
-import { Action } from "./types";
+import { Action, AsyncAction } from "./types";
 
 // Define your higher-order function
 export const createBufferize = (lock: Lock) => {
   const actionQueue = new ActionQueue();
-  const bufferize = ({ dispatch, getState, isProcessing, actionStack, dependencies } : any) => (next: Function) => async (action: Action<any>) => {
-
+  const thunk = ({dispatch, getState, dependencies, isProcessing, actionStack }: any) => (next: Function) => async (action: Action<any> | AsyncAction<any>) => {
     if (action instanceof Function) {
       return action(dispatch, getState, dependencies());
     }
 
     actionStack.push(action);
     return next(action);
+  };
+
+  const bufferize = ({ dispatch, getState, dependencies, isProcessing, actionStack } : any) => (next: Function) => async (action: Action<any>) => {
+    return thunk({ dispatch, getState, dependencies, isProcessing, actionStack })(next)(action);
   };
 
   // Return the bufferize middleware
