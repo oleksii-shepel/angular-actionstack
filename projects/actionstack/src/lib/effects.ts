e2c21304be82f32f35fc2fd4b0ffac026e31971b
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-02-12 14:24:30 +0200

    corrections in effect creator

--- projects/actionstack/src/lib/effects.ts
+++ projects/actionstack/src/lib/effects.ts
@@ -1,6 +1,6 @@
-import { Observable, isObservable, of } from 'rxjs';
-import { concatMap, filter, ignoreElements, withLatestFrom } from 'rxjs/operators';
-import { Action, SideEffect } from './types';
+import { Observable, OperatorFunction, concat, concatMap, filter, from, ignoreElements, isObservable, merge, mergeMap, of, toArray, withLatestFrom } from 'rxjs';
+import { Action, SideEffect, isAction } from "./types";
+
 
 export function createEffect(
   actionType: string,
@@ -26,3 +26,65 @@ export function createEffect(
       })
     );
 }
+
+
+export function ofType(...types: [string, ...string[]]): OperatorFunction<Action<any>, Action<any>> {
+  return filter((action): action is Action<any> => {
+    if (isAction(action)) {
+      return types.includes(action.type);
+    }
+    return false;
+  });
+}
+
+
+export function combine(strategy: 'exclusive' | 'concurrent', ...effects: SideEffect[]): SideEffect {
+  const merger: SideEffect = (...args) => {
+    const observables = effects.map((effect) => {
+      const output$ = effect(...args);
+      if (!output$) throw new TypeError(`combine: one of the provided effects "${effect.name || '<anonymous>'}" does not return a stream. Double check you're not missing a return statement!`);
+      return output$;
+    });
+
+    return strategy === 'concurrent' ? merge(...observables): concat(...observables);
+  };
+
+  try {
+    Object.defineProperty(merger, 'name', { value: `combine(${effects.map((effect) => effect.name || '<anonymous>').join(', ')})` });
+  } catch (e) {}
+
+  return merger;
+}
+
+
+
+export function runSideEffectsSequentially(sideEffects: SideEffect[], dependencies: Record<string, any>) {
+  return ([action$, state$]: [Observable<Action<any>>, Observable<any>]) =>
+    action$.pipe(
+      withLatestFrom(state$),
+      concatMap(([action, state]) =>
+        from(sideEffects).pipe(
+          concatMap((sideEffect: SideEffect) => sideEffect(action$, state$, dependencies))
+        )
+      ),
+      toArray()
+    );
+}
+
+
+export function runSideEffectsInParallel(sideEffects: SideEffect[], dependencies: Record<string, any>) {
+  return ([action$, state$]: [Observable<Action<any>>, Observable<any>]) =>
+    action$.pipe(
+      withLatestFrom(state$),
+      mergeMap(([action, state]) =>
+        from(sideEffects).pipe(
+          mergeMap((sideEffect: SideEffect) => sideEffect(action$, state$, dependencies))
+        )
+      ),
+      toArray()
+    );
+}
+
+
+
+
