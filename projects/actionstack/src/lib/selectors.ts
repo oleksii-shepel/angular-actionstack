bbbf4c7a32051c0700ce3a84660b0dedd727ba57
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-03-12 12:12:45 +0200

    modified createSelectorAsync

--- projects/actionstack/src/lib/selectors.ts
+++ projects/actionstack/src/lib/selectors.ts
@@ -1,5 +1,4 @@
-import { EMPTY, Observable, concatMap, distinctUntilChanged, map, of, shareReplay } from "rxjs";
-import { Store } from './store';
+import { EMPTY, Observable, concatMap, distinctUntilChanged, map, of, shareReplay, switchMap } from "rxjs";
 import { AnyFn, ProjectionFunction } from "./types";
 
 export function createFeatureSelector<U = any, T = any> (
@@ -53,10 +52,10 @@ export function createSelector<U = any, T = any> (
 }
 
 export function createSelectorAsync<U = any, T = any> (
-  slice: keyof T | string[],
+  featureSelector$: (store: Observable<T>) => Observable<U>,
   selectors: ((...args: any) => any | Promise<any>) | ((...args: any) => any | Promise<any>)[],
   projectionOrOptions?: ProjectionFunction
-): (props?: any[] | any, projectionProps?: any) => Promise<AnyFn> {
+): (props?: any[] | any, projectionProps?: any) => (store: Observable<T>) => Observable<U> {
 
   const isSelectorArray = Array.isArray(selectors);
   const projection = typeof projectionOrOptions === "function" ? projectionOrOptions : undefined;
@@ -65,34 +64,31 @@ export function createSelectorAsync<U = any, T = any> (
     throw new Error("Invalid parameters: When 'selectors' is an array, 'projection' function should be provided.");
   }
 
-  // The createSelectorAsync function will return a function that takes some arguments and returns combined result of selection and projection
-  return async (props?: any[] | any, projectionProps?: any) => {
-    if (Array.isArray(props) && Array.isArray(selectors) && props.length !== selectors.length) {
+  return (props?: any[] | any, projectionProps?: any) => {
+    if(Array.isArray(props) && Array.isArray(selectors) && props.length !== selectors.length) {
       throw new Error('Not all selectors are parameterized. The number of props does not match the number of selectors.');
     }
-    // The memoizedSelector function will return a function that executes the selectors and projection
-    const fn = async (store: Store): Promise<U> => {
-      let sliceState = await store.getState(slice);
-      if (sliceState === undefined) {
-        return undefined as U;
-      }
 
-      let selectorResults;
-      if (Array.isArray(selectors)) {
-        selectorResults = await Promise.allSettled(selectors.map(async (selector, index) => selector && await selector(sliceState, props && props[index])));
-        selectorResults = selectorResults.map(result => result.status === 'fulfilled' ? result.value : undefined);
-
-        return (selectorResults.some(result => typeof result === 'undefined'))
-          ? undefined as U
-          : projection ? projection(selectorResults, projectionProps) : undefined;
-      } else {
-        selectorResults = selectors && await selectors(sliceState, props);
-        return (typeof selectorResults === 'undefined')
-          ? undefined as U
-          : projection ? projection(selectorResults, projectionProps) : selectorResults;
-      }
+    return (state$: Observable<T>) => {
+      return featureSelector$(state$).pipe(
+        concatMap(async sliceState => {
+          let selectorResults;
+          if (Array.isArray(selectors)) {
+            // Use Promise.all to wait for all async selectors to resolve
+            selectorResults = await Promise.all(selectors.map((selector, index) => selector(sliceState, props[index])));
+            return of((selectorResults.some(result => typeof result === 'undefined'))
+              ? undefined as U
+              : projection ? projection(selectorResults, projectionProps) : selectorResults)
+          } else {
+            // If selectors is a single function, await its result
+            selectorResults = await selectors(sliceState, props);
+            return of((typeof selectorResults === 'undefined')
+              ? undefined as U
+              : projection ? projection(selectorResults, projectionProps) : selectorResults)
+          }
+        }),
+        switchMap(result => result) // switchMap to unwrap the Observable returned by of()
+      );
     };
-
-    return fn;
   };
 }
