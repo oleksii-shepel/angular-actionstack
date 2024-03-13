ecb2806ea8a677320cb682f17a95be1ff8899d6e
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-03-13 07:10:45 +0200

    removed obsolete code

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,28 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
-import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
 import { Action, StoreModule, measure } from 'actionstack';
 import { AppRoutingModule } from './app-routing.module';
 import { MessagesModule } from './messages/messages.module';
 
-
-export function getBaseHref(platformLocation: PlatformLocation): string {
-  return platformLocation.getBaseHrefFromDOM();
-}
-
-
 @NgModule({
-  providers: [
-    {
-      provide: APP_BASE_HREF,
-      useFactory: getBaseHref,
-      deps: [PlatformLocation],
-    },
-  ],
   imports: [
     BrowserModule,
     FormsModule,
