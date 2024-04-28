4859e74cbfbf56750a702ab40994b738d6fe5647
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-04-28 14:49:08 +0300

    Update starter.ts

--- projects/actionstack/src/lib/starter.ts
+++ projects/actionstack/src/lib/starter.ts
@@ -22,7 +22,7 @@ export const createStarter = () => {
    * @param next - Function to call the next middleware in the chain.
    * @returns Function - The actual middleware function that handles actions.
    */
-  const exclusive = ({ dispatch, getState, dependencies, isProcessing, lock }: any) => (next: Function) => async (action: Action<any> | AsyncAction<any>) => {
+  const exclusive = ({ dispatch, getState, dependencies, isProcessing }: any) => (next: Function) => async (action: Action<any> | AsyncAction<any>) => {
     async function processAction(action: Action<any> | AsyncAction<any>) {
       if (typeof action === 'function') {
         // Process async actions (functions)
@@ -31,16 +31,11 @@ export const createStarter = () => {
         // Pass regular actions to the next middleware
         isProcessing.next(true);
         await next(action);
-        await waitFor(isProcessing, value => value === false);
       }
     }
 
-    await lock.acquire()
-    try {
-      await processAction(action);
-    } finally {
-      lock.release();
-    }
+    await waitFor(isProcessing, value => value === false);
+    await processAction(action);
   };
 
   /**
@@ -52,7 +47,7 @@ export const createStarter = () => {
    * @param next - Function to call the next middleware in the chain.
    * @returns Function - The actual middleware function that handles actions.
    */
-  const concurrent = ({ dispatch, getState, dependencies, isProcessing, lock }: any) => (next: Function) => async (action: Action<any> | AsyncAction<any>) => {
+  const concurrent = ({ dispatch, getState, dependencies, isProcessing }: any) => (next: Function) => async (action: Action<any> | AsyncAction<any>) => {
     async function processAction(action: Action<any> | AsyncAction<any>) {
       if (typeof action === 'function') {
         // Process async actions asynchronously and track them
@@ -67,16 +62,11 @@ export const createStarter = () => {
         // Pass regular actions to the next middleware
         isProcessing.next(true);
         await next(action);
-        await waitFor(isProcessing, value => value === false);
       }
     }
 
-    await lock.acquire()
-    try {
-      await processAction(action);
-    } finally {
-      lock.release();
-    }
+    await waitFor(isProcessing, value => value === false);
+    await processAction(action);
   };
 
   // Map strategy names to functions
@@ -86,12 +76,12 @@ export const createStarter = () => {
   };
 
   // Create a method to select the strategy
-  const selectStrategy = ({ dispatch, getState, dependencies, isProcessing, strategy, lock }: any) => (next: Function) => async (action: Action<any>) => {
+  const selectStrategy = ({ dispatch, getState, dependencies, isProcessing, strategy }: any) => (next: Function) => async (action: Action<any>) => {
     const strategyFunc = strategies[strategy()];
     if (!strategyFunc) {
       throw new Error(`Unknown strategy: ${strategy}`);
     }
-    return strategyFunc({ dispatch, getState, dependencies, isProcessing, lock })(next)(action);
+    return strategyFunc({ dispatch, getState, dependencies, isProcessing })(next)(action);
   };
 
   selectStrategy.signature = 'i.p.5.j.7.0.2.1.8.b';
