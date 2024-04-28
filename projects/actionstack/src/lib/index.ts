7aaebb79239a541aceaa3f7318f9973ea0238335
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-04-28 14:44:05 +0300

    Update index.ts

--- projects/actionstack/src/lib/index.ts
+++ projects/actionstack/src/lib/index.ts
@@ -1,7 +1,6 @@
 export * from "./actions";
 export * from "./effects";
 export * from "./hash";
-export * from "./lock";
 export * from "./module";
 export * from "./observable";
 export * from "./operators";
