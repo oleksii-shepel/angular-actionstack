743cafa6f6e67e739c03924f5cf25642b18ceeea
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-04-30 11:47:31 +0300

    corrections in slice

--- projects/actionstack/src/lib/slice.ts
+++ projects/actionstack/src/lib/slice.ts
@@ -61,8 +61,8 @@ export class Slice implements OnDestroy {
    */
   setup(opts: SliceOptions): void {
     this.opts = Object.assign(this.opts, opts);
-    this.opts.effects && this.opts.effects.length && (this.subscription = this.store.extend(...this.opts.effects as any).subscribe());
-    this.opts.slice && this.opts.reducer && this.store.loadModule({
+    this.opts.effects && this.opts.effects.length > 0 && (this.subscription = this.store.extend(...this.opts.effects as any).subscribe());
+    this.opts.slice !== undefined && this.opts.reducer && this.store.loadModule({
       slice: this.opts.slice,
       dependencies: this.opts.dependencies,
       reducer: this.opts.reducer
@@ -94,6 +94,6 @@ export class Slice implements OnDestroy {
    */
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
-    this.opts.slice && this.opts.reducer && this.store.unloadModule({slice: this.opts.slice, reducer: this.opts.reducer}, this.opts.strategy === "temporary" ? true : false);
+    this.opts.slice !== undefined && this.opts.reducer && this.store.unloadModule({slice: this.opts.slice, reducer: this.opts.reducer}, this.opts.strategy === "temporary" ? true : false);
   }
 }
