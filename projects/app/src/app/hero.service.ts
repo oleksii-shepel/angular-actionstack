a8bab78d823363794ff70fbc381eb17dffa406d9
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-04-30 13:41:26 +0300

    corrections in heroService due to dispatching multiple actions

--- projects/app/src/app/hero.service.ts
+++ projects/app/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Observable, from } from 'rxjs';
+import { Observable, from, tap } from 'rxjs';
 
 import { Store } from '@actioncrew/actionstack';
 import { Hero } from './hero';
@@ -16,20 +16,21 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return from(new Promise<Hero[]>((resolve) => {
       setTimeout(() => {
-        this.store.dispatch(addMessage('HeroService: fetched heroes'));
         resolve(HEROES);
       }, this.timeout);
-    }));
+    })).pipe(
+      tap(() => this.store.dispatch(addMessage('HeroService: fetched heroes')))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
     return from(new Promise<Hero>((resolve) => {
       setTimeout(() => {
         const hero = HEROES.find(h => h.id === id)!;
-
-        this.store.dispatch(addMessage(`HeroService: fetched hero id=${id}`));
         resolve(hero);
       }, this.timeout);
-    }));
+    })).pipe(
+      tap(() => this.store.dispatch(addMessage(`HeroService: fetched hero id=${id}`)))
+    );
   }
 }
