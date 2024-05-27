ebdd7e3cd8658ad9ad05836a8651fcfec11d4b5e
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-05-27 17:24:14 +0300

    Update sagas.ts

--- projects/actionstack/sagas/src/lib/sagas.ts
+++ projects/actionstack/sagas/src/lib/sagas.ts
@@ -34,10 +34,10 @@ export const createSagasMiddleware = ({
               throw new Error('saga argument must be a Generator function!');
             }
 
-            const op = {operation: OperationType.SAGA, instance: saga};
-            stack.push(op); Object.assign(context, dependencies());
             const task: Task = runSaga({ context, channel, dispatch: customDispatch(middlewareDispatch)(op), getState: middlewareGetState }, (function*(): Generator<any, void, any> {
               try {
+                const op = {operation: OperationType.SAGA, instance: saga};
+                stack.push(op); Object.assign(context, dependencies());
                 yield call(saga);
               } catch (error) {
                 console.error('Saga error:', error);
