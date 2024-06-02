045b23e72bf7fcfcf464524bda9290a9123242dd
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-06-02 16:12:14 +0300

    Update epics.ts

--- projects/actionstack/epics/src/lib/epics.ts
+++ projects/actionstack/epics/src/lib/epics.ts
@@ -1,13 +1,10 @@
 import { Action, action, Epic, isAction } from '@actioncrew/actionstack';
-import { Subscription } from 'rxjs';
 import { Subject } from 'rxjs/internal/Subject';
-
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { Observable } from 'rxjs/internal/Observable';
 import { Subscription } from 'rxjs/internal/Subscription';
 
 import { ExecutionStack, OperationType } from './stack';
-import { Action, isAction } from './types';
 
 /**
  * Projects each source value to a Promise which is merged in the output Observable
@@ -156,42 +153,6 @@ export function merge<T>(stack: ExecutionStack, ...sources: Observable<T>[]): Ob
         subscriptions = [];
       }
     };
-  });
-}
-
-/**
- * Waits for a condition to be met in an observable stream.
- * @param {Observable<any>} obs - The observable stream to wait for.
- * @param {(value: any) => boolean} predicate - The predicate function to evaluate the values emitted by the observable stream.
- * @returns {Promise<boolean>} A promise that resolves to true when the predicate condition is met, or false if the observable completes without satisfying the predicate.
- */
-export function waitFor<T>(obs: Observable<T>, predicate: (value: T) => boolean): Promise<T> {
-  let subscription: Subscription | undefined;
-
-  return new Promise<T>((resolve, reject) => {
-    const checkInitialValue = (obs as BehaviorSubject<T>)?.value;
-    if (checkInitialValue !== undefined && predicate(checkInitialValue)) {
-      return resolve(checkInitialValue);
-    }
-
-    subscription = obs.subscribe({
-      next: value => {
-        if (predicate(value)) {
-          if (subscription) {
-            subscription.unsubscribe();
-          }
-          resolve(value);
-        }
-      },
-      error: err => reject(err),
-      complete: () => {
-        reject("Method had completed before predicate condition was met");
-      },
-    });
-  }).finally(() => {
-    if (subscription && !subscription.closed) {
-      subscription.unsubscribe();
-    }
   });
 }
 
