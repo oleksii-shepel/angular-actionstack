a29a837e796f14c95d9c48c90f2fa1b8a7f32810
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-06-19 14:25:29 +0300

    reordering of exports

--- projects/actionstack/src/lib/index.ts
+++ projects/actionstack/src/lib/index.ts
@@ -4,11 +4,11 @@ export * from "./lock";
 export * from "./module";
 export * from "./selectors";
 export * from "./slice";
+export * from "./stack";
 export * from "./starter";
 export * from "./store";
 export * from "./tracker";
 export * from "./types";
-export * from "./stack";
 
 
 
